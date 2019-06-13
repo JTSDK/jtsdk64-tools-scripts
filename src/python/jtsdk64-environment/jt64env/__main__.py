f043a5c75de42eefdf1cda3ff056954d8a951433
ki7mt <ki7mt@yahoo.com>
2019-06-13 15:45:42 -0600

    Move consolecolor values to seperate file

--- src/python/jtsdk64-environment/jt64env/__main__.py
+++ src/python/jtsdk64-environment/jt64env/__main__.py
@@ -4,27 +4,6 @@ import sys
 from colorconsole import terminal
 
 
-"""[Console Color Index]
-    Number  Color
-    0 	    Black
-    1 	    Blue
-    2 	    Green
-    3 	    Cyan
-    4 	    Red
-    5 	    Purple
-    6 	    Brown
-    7 	    Light gray
-    8 	    Dark gray
-    9 	    Light blue
-    10 	    Light Green
-    11 	    Light Cyan
-    12 	    Light Red
-    13 	    Light Purple
-    14 	    Yellow
-    15 	    White
-"""
-
-
 def clear():
     """Clear screen Windows or *Nix"""
     os.system('cls' if os.name == 'nt' else 'clear')
