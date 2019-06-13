faae04fe73e0808acfe6e7cbbf071e3633f54264
ki7mt <ki7mt@yahoo.com>
2019-06-13 14:14:52 -0600

    Fix missing __title__ variable in __init__.py

--- src/python/jtsdk64-environment/jt64env/__init__.py
+++ src/python/jtsdk64-environment/jt64env/__init__.py
@@ -1,10 +1,18 @@
 # Package meta-data
 version_info = (0, 0, 1)
+__title__ = "JTSDK64 Tools Environment"
 __version__ = '.'.join(str(c) for c in version_info)
-__name__ = "jtenv"
+__name__ = "jt64env"
 __author__ = "Greg Beam, KI7MT"
 __license__ = "GPLv3"
 __email__ = "ki7mt@yahoo.com"
 
 # All meta-data
-__all__ = ['__version__', '__name__', '__author__', '__license__', '__email__']
+__all__ = [
+            '__title__',
+            '__version__',
+            '__name__',
+            '__author__',
+            '__license__',
+            '__email__'
+            ]
