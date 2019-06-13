887526e75817ff3be177bb4cd9e3762ed5f135c8
ki7mt <ki7mt@yahoo.com>
2019-06-13 11:36:34 -0600

    Add doc strics, TODO items, and fix pep8 lint issues

--- src/python/jtsdk64-help/jt64help/__main__.py
+++ src/python/jtsdk64-help/jt64help/__main__.py
@@ -1,6 +1,9 @@
-import os, sys
+import os
+import sys
 from colorconsole import terminal
 
+
+# TODO: Move listing to a common text file
 """[Console Color Index]
     Number  Color
     0 	    Black
@@ -20,15 +23,20 @@ from colorconsole import terminal
     14 	    Yellow
     15 	    White
 """
+
+
+# TODO: Move to jt64common package
 def clear():
+    """Clear Screen WIndows of *Nix"""
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def main():
+    """Prints Main Help Message"""
     clear()
     screen = terminal.get_terminal(conEmu=False)
     print("------------------------------------------------------------")
-    screen.set_color(3,0)
+    screen.set_color(3, 0)
     print(f"JTSDK64 Tools {os.environ['VERSION']} Help Menu")
     screen.reset_colors()
     print("------------------------------------------------------------")
@@ -36,16 +44,15 @@ def main():
     print("The following commands are available throughout the JTSDK64")
     print("Tools Environment.")
     print("\nScripts Commands\n")
-    print('{0:15}  {1}'.format("  jtsdk-help", "Print This Help Menu"))
-    print('{0:15}  {1}'.format("  env-check", "Lists All Environment Variables"))
-    print('{0:15}  {1}'.format("  gentc", "Generate Tool Chain Files"))
-    print('{0:15}  {1}'.format("  version-check", "Checks Tool-Chain Versions"))
+    print('{0:15}  {1}'.format("  jt64help", "Print This Help Menu"))
+    print('{0:15}  {1}'.format("  jt64en", "Lists All Environment Variables"))
+    print('{0:15}  {1}'.format("  jt64gentc", "Generate Tool Chain Files"))
+    print('{0:15}  {1}'.format("  jt64version", "Checks Tool-Chain Versions"))
     print("\nShortcut Commands\n")
     print('{0:15}  {1}'.format("  home", "Return back to home directory"))
     print('{0:15}  {1}'.format("  msys2", "Launch MSYS2 Console"))
 
+
 if __name__ == '__main__':
     main()
     sys.exit(0)
-
-# End jtsdk-env-message.py
